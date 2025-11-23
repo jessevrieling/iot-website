5d2bc64dfc7041831830c97a67348936ebfc634d
Jesse <jessevrieling@gmail.com>
2025-11-23 15:06:41 +0100

    added endpoint for fetching measurements

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template,
+import mysql.connector
 import random
 
 app = Flask(__name__)
 
 @app.route("/measurements", methods=["GET"])
+def measurements():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="iotdb"
+    )
+
+    dbcursor = db.cursor(dictionary=True);
+    dbcursor.execute("SELECT * FROM `measurement` ORDER BY timestamp DESC LIMIT 20")
+    records = dbcursor.fetchall()
+
+    return jsonify(records)
 
 @app.route('/', methods=["GET"])
 def dashboard():
